d552b66036bba87518f88901c5cf6de97414d407
swj <1061891980@qq.com>
2024-03-25 20:02:51 +0800

    init 初步提交

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,16 +1,16 @@
-const sidebar = require('./config/sidebar/index.js');
-const nav = require('./config/nav/index.js')
+const sidebar = require("./config/sidebar/index.js");
+const nav = require("./config/nav/index.js");
 module.exports = {
-    title: 'ssswj',
-    description: 'It is a start',
-    theme: 'reco',
-    themeConfig: {
-        type: 'blog',
-        authorAvatar: '/home/avatar.jpeg',
-        nav,
-        modePicker: false,
-        mode: 'dark',
-        sidebar,
-        // lastUpdated: 'Last Updated',
-    },
-  }
+  title: "ssswj",
+  description: "It is a start",
+  theme: "reco",
+  themeConfig: {
+    type: "blog",
+    authorAvatar: "/home/avatar.jpeg",
+    nav,
+    modePicker: false,
+    mode: "dark",
+    sidebar,
+    lastUpdated: "Last Updated",
+  },
+};
